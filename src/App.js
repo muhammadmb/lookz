7380fd73e8414c7f87397190454e4ecf1ecbb4a7
muhammadmb <muhammadmagdyje@gmail.com>
2022-01-09 21:28:53 +0200

    design Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Home from './Components/Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+
       </BrowserRouter>
 
     </div>
